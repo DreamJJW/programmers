d4149c1941537853449e5cd710f6de2405abf26a
DreamJJW <chiwoojw@gmail.com>
2023-01-04 15:36:53 +0900

    .

---
+++ 모스부호(1).py
@@ -0,0 +1,17 @@
+def solution(letter):
+    answer = ''
+    letter = list(letter.split())
+    # 모스 부호
+    morse = {
+        '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+        '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+        '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
+        '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+        '-.--': 'y', '--..': 'z'
+    }
+    for i in letter:
+        answer += morse.get(i)
+    print(answer)
+    return answer
+
+solution(letter=".--. -.-- - .... --- -.")
