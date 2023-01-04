ba1f0d461c7fc80b893216d336f20c7c71b7fd28
DreamJJW <chiwoojw@gmail.com>
2023-01-04 10:43:49 +0900

    .

--- 진료 순서 정하기.py
+++ 진료 순서 정하기.py
@@ -1,6 +1,7 @@
 def solution(emergency):
     temp = emergency
     answer = []
+    # sort는 정렬된 리스트를 반환 , sorted는 반환하지 않음
     emergency = sorted(emergency, reverse=True)
     dic = {string: i + 1 for i, string in enumerate(emergency)}
     print(dic)
