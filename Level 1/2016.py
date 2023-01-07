928f7792ba92724e33844014bfe8b4717ceb46ae
DreamJJW <chiwoojw@gmail.com>
2023-01-08 02:35:06 +0900

    .

--- Level 1/2016.py
+++ Level 1/2016.py
@@ -1,1 +1,13 @@
-.
+def solution(a, b):
+    days = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+    # 2월은 29일 (윤년)
+    day = 0
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for i in range(a - 1):
+        day += months[i]
+    day += b
+    days = days * 52
+    print(days[day - 1])
+
+
+solution(a = 5, b = 24)
